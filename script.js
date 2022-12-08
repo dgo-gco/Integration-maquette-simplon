ef64dbfdd866f350d435ebc0bc64207200ca49ba
Diego <110729317+dgo-gco@users.noreply.github.com>
2022-12-08 22:48:33 +0100

    finishing responsive design

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const navMenu = document.querySelector('.nav-bar')
 
 const menuBtn = document.querySelector('.nav-bar');
 
+const temoignageDroite = document.querySelector('.');
+const temoignageGauche = document.querySelector('.');
+
+const soutienDroite = document.querySelector('.');
+const soutienGauche = document.querySelector('.');
+
+
 menu.addEventListener('click', function(){
     navMenu.classList.toggle('mobile-menu');
 })
